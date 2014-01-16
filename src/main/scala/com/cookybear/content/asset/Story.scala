1db6b138c9a541095d5a39ea33c7491b7ed71750
Jon Cook <jonathan.cook@erars.plus.com>
2014-01-16 23:34:54 +0100

    Pretty print files

--- src/main/scala/com/cookybear/content/asset/Story.scala
+++ src/main/scala/com/cookybear/content/asset/Story.scala
@@ -1,24 +1,32 @@
 package com.cookybear.content.asset
 
-class Story(itemMeta: ItemMeta, pageOptions: PageOptions, byline: Byline, body: scala.xml.NodeSeq, media: Media, relatedGroups: RelatedGroups) extends Asset(itemMeta, pageOptions) {
+import scala.xml.NodeSeq
+
+class Story(
+  itemMeta: ItemMeta,
+  pageOptions: PageOptions,
+  byline: Byline,
+  body: NodeSeq,
+  media: Media,
+  relatedGroups: RelatedGroups) extends Asset(itemMeta, pageOptions) {
 
   override def toXML =
-	<result>  
-  		{super.toXML.child}
-  		{byline.toXML}
-  		{body}
-  		{media.toXML}
-  		{relatedGroups.toXML}
-  	</result>
+    <result>
+      { super.toXML.child }
+      { byline.toXML }
+      { body }
+      { media.toXML }
+      { relatedGroups.toXML }
+    </result>
 }
 
 object Story {
-  def fromXML(node: scala.xml.NodeSeq): Story =
+  def fromXML(node: NodeSeq): Story =
     new Story(
-        ItemMeta.fromXML((node \ "itemMeta")), 
-        PageOptions.fromXML((node \ "pageOptions")),
-        Byline.fromXML((node \ "byline")),
-  		(node \ "body"),
-        Media.fromXML((node \ "media")),
-  		RelatedGroups.fromXML(node \ "relatedGroups"))
+      ItemMeta.fromXML((node \ "itemMeta")),
+      PageOptions.fromXML((node \ "pageOptions")),
+      Byline.fromXML((node \ "byline")),
+      (node \ "body"),
+      Media.fromXML((node \ "media")),
+      RelatedGroups.fromXML(node \ "relatedGroups"))
 }
--- src/main/scala/com/cookybear/content/asset/factory/AssetFactory.scala
+++ src/main/scala/com/cookybear/content/asset/factory/AssetFactory.scala
@@ -4,14 +4,14 @@ import com.cookybear.content.asset.Asset
 import com.cookybear.content.asset.Story
 
 object AssetFactory {
-  
+
   def factory(node: scala.xml.Node): Asset = {
     val trimmedNode = scala.xml.Utility.trim(node)
-    
+
     trimmedNode match {
-      case <story>{children @ _*}</story> => Story.fromXML(trimmedNode)
+      case <story>{ children @ _* }</story> => Story.fromXML(trimmedNode)
     }
-    
+
   }
-  
+
 }
